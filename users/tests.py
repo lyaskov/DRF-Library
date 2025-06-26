57a7420b1f11fd6a3ed67f2693d615e99045729a
lyaskov <lyaskov1@gmail.com>
2025-06-26 19:50:49 +0300

    add UserTests class with setup for API tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from users.models import User
+
+REGISTER_URL = reverse("user_register")
+ME_URL = reverse("user_me")
+TOKEN_URL = reverse("token_obtain_pair")
+
+
+class UserTests(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email="test@example.com",
+            password="password123",
+            first_name="Test",
+            last_name="User",
+        )
