430b94d459ff3072ca2e4c168443e2296f206e75
lyaskov <lyaskov1@gmail.com>
2025-06-26 19:15:27 +0300

    add user registration and user profile (me) endpoints

--- users/urls.py
+++ users/urls.py
@@ -5,10 +5,13 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
+from users.views import RegisterUserView, MeView
 
 app_name = "user"
 
 urlpatterns = [
+    path("register/", RegisterUserView.as_view(), name="user_register"),
+    path("me/", MeView.as_view(), name="user_me"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
+from users.serializers import UserSerializer
+
 
 # Create your views here.
+class RegisterUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class MeView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
