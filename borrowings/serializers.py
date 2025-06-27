e7adc90662a698b730a7dcc195971dc3dbebf4ea
lyaskov <lyaskov1@gmail.com>
2025-06-27 19:14:57 +0300

    Implement the Create Borrowing

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from rest_framework import serializers
 
 from books.serializers import BookSerializer
@@ -16,3 +18,31 @@ class BorrowingReadSerializer(serializers.ModelSerializer):
             "expected_return_date",
             "actual_return_date",
         ]
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = ("book", "expected_return_date")
+
+    def validate(self, attrs):
+        book = attrs["book"]
+
+        if book.inventory < 1:
+            raise serializers.ValidationError("Book is not available for borrowing.")
+
+        if attrs["expected_return_date"] < date.today():
+            raise serializers.ValidationError(
+                "Expected return date cannot be in the past."
+            )
+
+        return attrs
+
+    def create(self, validated_data):
+        user = self.context["request"].user
+        book = validated_data["book"]
+
+        book.inventory -= 1
+        book.save()
+
+        return Borrowing.objects.create(user=user, **validated_data)
--- borrowings/views.py
+++ borrowings/views.py
@@ -1,23 +1,18 @@
-from rest_framework import generics
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingReadSerializer
+from borrowings.serializers import BorrowingReadSerializer, BorrowingCreateSerializer
 
 
-class BorrowingListView(generics.ListAPIView):
+class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
-    serializer_class = BorrowingReadSerializer
     permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
-        return Borrowing.objects.filter(user=self.request.user)
-
-
-class BorrowingDetailView(generics.RetrieveAPIView):
-    queryset = Borrowing.objects.all()
-    serializer_class = BorrowingReadSerializer
-    permission_classes = [IsAuthenticated]
+    def get_serializer_class(self):
+        if self.action == "create":
+            return BorrowingCreateSerializer
+        return BorrowingReadSerializer
 
     def get_queryset(self):
         return Borrowing.objects.filter(user=self.request.user)
