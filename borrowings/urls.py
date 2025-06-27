b9d38a5fd993ecbddb431f20a1ee0c080b87da9e
lyaskov <lyaskov1@gmail.com>
2025-06-27 17:44:00 +0300

    Refactor url endpoint

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -5,8 +5,6 @@ from borrowings.views import BorrowingListView, BorrowingDetailView
 app_name = "borrowings"
 
 urlpatterns = [
-    path("borrowings/", BorrowingListView.as_view(), name="borrowing-list"),
-    path(
-        "borrowings/<int:pk>/", BorrowingDetailView.as_view(), name="borrowing-detail"
-    ),
+    path("", BorrowingListView.as_view(), name="borrowing-list"),
+    path("<int:pk>/", BorrowingDetailView.as_view(), name="borrowing-detail"),
 ]
