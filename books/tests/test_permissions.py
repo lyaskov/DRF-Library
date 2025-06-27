2baeba1e2da0f6345ffb883014a7e361d112dcc2
lyaskov <lyaskov1@gmail.com>
2025-06-27 11:37:15 +0300

    Use authentication from base test class

--- books/tests/test_permissions.py
+++ books/tests/test_permissions.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 from books.models import Book
+from books.tests.base import AuthenticatedAPITestCase
 
 BOOKS_URL = reverse("books:book-list")
 User = get_user_model()
@@ -13,14 +14,10 @@ def detail_url(book_id):
     return reverse("books:book-detail", args=[book_id])
 
 
-class BookPermissionsTests(APITestCase):
+class BookPermissionsTests(AuthenticatedAPITestCase):
     def setUp(self):
-        self.staff_user = User.objects.create_user(
-            email="admin@example.com", password="adminpass", is_staff=True
-        )
-        self.normal_user = User.objects.create_user(
-            email="user@example.com", password="userpass", is_staff=False
-        )
+        self.staff_user = self.get_staff_user()
+        self.normal_user = self.get_normal_user()
         self.book = Book.objects.create(
             title="Test Book",
             author="Author",
@@ -28,18 +25,6 @@ class BookPermissionsTests(APITestCase):
             inventory=3,
             daily_fee="1.99",
         )
-
-    def authenticate(self, user):
-        res = self.client.post(
-            reverse("user:token_obtain_pair"),
-            {
-                "email": user.email,
-                "password": "adminpass" if user.is_staff else "userpass",
-            },
-            format="json",
-        )
-        token = res.data["access"]
-        self.client.credentials(HTTP_AUTHORIZE=f"Bearer {token}")
 
     def test_unauthenticated_user_can_list_books(self):
         res = self.client.get(BOOKS_URL)
@@ -60,7 +45,7 @@ class BookPermissionsTests(APITestCase):
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_authenticated_non_staff_user_cannot_create_book(self):
-        self.authenticate(self.normal_user)
+        self.authenticate_normal_user()
         res = self.client.post(
             BOOKS_URL,
             {
@@ -75,7 +60,7 @@ class BookPermissionsTests(APITestCase):
         self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_authenticated_staff_user_can_create_book(self):
-        self.authenticate(self.staff_user)
+        self.authenticate_staff_user()
         res = self.client.post(
             BOOKS_URL,
             {
@@ -90,7 +75,7 @@ class BookPermissionsTests(APITestCase):
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
 
     def test_authenticated_staff_user_can_update_book(self):
-        self.authenticate(self.staff_user)
+        self.authenticate_staff_user()
         url = detail_url(self.book.id)
         res = self.client.patch(url, {"inventory": 10}, format="json")
 
@@ -100,7 +85,7 @@ class BookPermissionsTests(APITestCase):
         self.assertEqual(self.book.inventory, 10)
 
     def test_non_staff_user_cannot_update_book(self):
-        self.authenticate(self.normal_user)
+        self.authenticate_normal_user()
         url = detail_url(self.book.id)
         res = self.client.patch(url, {"inventory": 999}, format="json")
 
@@ -110,13 +95,13 @@ class BookPermissionsTests(APITestCase):
         self.assertNotEqual(self.book.inventory, 999)
 
     def test_non_staff_user_cannot_delete_book(self):
-        self.authenticate(self.normal_user)
+        self.authenticate_normal_user()
         url = detail_url(self.book.id)
         res = self.client.delete(url)
         self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_staff_user_can_delete_book(self):
-        self.authenticate(self.staff_user)
+        self.authenticate_staff_user()
         url = detail_url(self.book.id)
         res = self.client.delete(url)
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
